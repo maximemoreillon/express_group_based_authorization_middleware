11271adc6abdded125d7582034416e8df8feb85d
Maxime Moreillon <m.moreillon@gmail.com>
2021-04-04 01:29:45 +0000

    init

---
+++ index.js
@@ -0,0 +1,90 @@
+const axios = require('axios')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const retrieve_jwt = (req, res) => {
+  /*
+  Retrieving the token from either cookies or authorization header
+  */
+
+  let jwt = undefined
+
+  // See if jwt available from authorization header
+  if(!jwt){
+    if(('authorization' in req.headers)) {
+      jwt = req.headers.authorization.split(" ")[1]
+    }
+  }
+
+  // Try to get JWT from cookies
+  if(!jwt) {
+    const cookies = new Cookies(req, res)
+    jwt = cookies.get('jwt')
+  }
+
+  // Could think of throwing an error if no JWT
+
+  return jwt
+}
+
+module.exports = (options) => {
+  return (req, res, next) => {
+
+    const jwt = retrieve_jwt(req, res)
+
+    // if no JWT available, reject request
+    if(!jwt) {
+      console.log('[Auth middleware] JWT not found in either cookies or authorization header')
+      res.status(403).send('JWT not found in either cookies or authorization header')
+      return
+    }
+
+    const user_id = res.locals.user?.identity.low
+      || res.locals.user?.identity
+      || res.locals.user?._id
+
+    if(!user_id) {
+      console.log('[Auth middleware] JWT not found in either cookies or authorization header')
+      res.status(403).send('JWT not found in either cookies or authorization header')
+      return
+    }
+
+    const {group_id} = options
+
+    const group_manager_url = options.group_manager_url || process.env.GROUP_MANAGER_API_URL || 'http://group-manager'
+
+    const url = `${group_manager_url}/users/${user_id}/groups`
+
+    const headers = { Authorization: `Bearer ${jwt}` }
+
+    axios.get(url, {headers})
+    .then((response) => {
+
+      const groups_of_user = response.data
+
+      const user_is_member = records.find(record => {
+        const user_group = record._fields[record._fieldLookup.group]
+        const user_group_id = user_group.identity.low || user_group.identity
+        return user_group_id === group_id
+      })
+
+      if(user_is_member) next()
+      else {
+        const message = `User ${user_id} is not a member of group ${group_id}`
+        console.log(`[Auth middleware] ${message}`)
+        res.status(403).send(message)
+        return
+      }
+
+    })
+    .catch( error => {
+      // In case the request fails, forward error message
+      console.log(error)
+      res.status(403).send(error)
+    })
+
+
+    next()
+  }
+}
